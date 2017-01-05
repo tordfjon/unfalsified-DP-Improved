2b99cba0df9ba80af2fa7b0ef8bd0f9a6ce14d87
tordfjon <tordfjon@gmail.com>
2017-01-05 16:28:14 +0100

    Added case for LICLA in the supervisor whilst keeping e-hysteresis switching. This change can be moved to a masked controller near the end of the project. Ran simulation to verify correct implementation but simulation only go so far in confirming as the plant show a destabilizing behaviour. This is due to frs growing unbounded for the destabilizing controller.

--- Supervisor_detectable.m
+++ Supervisor_detectable.m
@@ -27,7 +27,7 @@ if flag == 0
    sizes.NumSampleTimes = 1;                        %Number of sample times
    sys = simsizes(sizes);
    
-%    switching_eps    = 20;
+   u_dat.switching_method = 'LICLA';
    u_dat.J     = zeros(u_dat.total_number,1);
    u_dat.index = [1:u_dat.total_number]';
 
@@ -41,6 +41,11 @@ if flag == 0
    u_dat.count = 0;
    u_dat.t_switch = 0;
    
+        %% Variables for LICLA
+    u_dat.eta   = 1;
+    u_dat.deta  = 0.5;
+    u_dat.unfalsified_controller_index = u_dat.index;
+   
    str = []; ts  = [delta 0];% sample time is delta
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -52,28 +57,53 @@ elseif flag == 2
     u_dat.count = u_dat.count + 1;
     
     u_dat.J = u;                             %% Input u(n) is the cost of n th Candidate Controller
-    j=0;                                     %% Initilize the number of Unfalsified Controller
-    p = u_dat.J(u_dat.cur_index);            %% Cost of current Controller
-    unfalsified_ball_index = [];
-
-    for i=1:u_dat.total_number               % u_dat.m - # of unfalsified controller candidates
-        if u_dat.J(i)+rho < p        %% if ith controller is unfalsified
-            j = j+1;                         %% j : # of unfalsified Controllers
-            unfalsified_ball_index(j) = i;
-        else 
-            % Do nothing
-        end
-    end
     
-    time_since_last = t-u_dat.t_switch;
-    if isempty(unfalsified_ball_index) %|| time_since_last < 1% i.e. no controller has better performance including hysteresis step
-        % Algorithm does not change controller
-    else % The unfalsified ball index set is non-empty, therfore a new controller is chosen
-        [~, next_controller] = min(u_dat.J(unfalsified_ball_index));   % New Controller selection Criterion
-        [u_dat.kp, u_dat.ki, u_dat.kd] = set_K_parameter(u_dat.K, unfalsified_ball_index, next_controller);
-        u_dat.prev_index = u_dat.cur_index; 
-        u_dat.cur_index = unfalsified_ball_index(next_controller);
-        u_dat.t_switch = t;
+    switch u_dat.switching_method
+        case 'epsilon'
+            j=0;                                     %% Initilize the number of Unfalsified Controller
+            p = u_dat.J(u_dat.cur_index);            %% Cost of current Controller
+            unfalsified_ball_index = [];
+            
+            for i=1:u_dat.total_number               % u_dat.m - # of unfalsified controller candidates
+                if u_dat.J(i)+rho < p        %% if ith controller is unfalsified
+                    j = j+1;                         %% j : # of unfalsified Controllers
+                    unfalsified_ball_index(j) = i;
+                else
+                    % Do nothing
+                end
+            end
+            
+            time_since_last = t-u_dat.t_switch;
+            if isempty(unfalsified_ball_index) %|| time_since_last < 1% i.e. no controller has better performance including hysteresis step
+                % Algorithm does not change controller
+            else % The unfalsified ball index set is non-empty, therfore a new controller is chosen
+                [~, next_controller] = min(u_dat.J(unfalsified_ball_index));   % New Controller selection Criterion
+                [u_dat.kp, u_dat.ki, u_dat.kd] = set_K_parameter(u_dat.K, unfalsified_ball_index, next_controller);
+                u_dat.prev_index = u_dat.cur_index;
+                u_dat.cur_index = unfalsified_ball_index(next_controller);
+                u_dat.t_switch = t;
+            end
+    
+        case 'LICLA' %LICLA with only self-falsification
+            % Do nothing as for now
+            if u_dat.J(u_dat.cur_index) > u_dat.eta
+                temp = u_dat.unfalsified_controller_index(u_dat.unfalsified_controller_index ~= u_dat.cur_index);
+                if isempty(temp) % Reinitialize falsified controllers that might performe better with varying system
+                    temp = u_dat.index;
+                end
+                u_dat.unfalsified_controller_index = temp;
+                
+                [~, next_controller] = min(u_dat.J(u_dat.unfalsified_controller_index));   % New Controller selection Criterion
+                [u_dat.kp, u_dat.ki, u_dat.kd] = set_K_parameter(u_dat.K, u_dat.unfalsified_controller_index, next_controller);
+                u_dat.prev_index = u_dat.cur_index;
+                u_dat.cur_index = u_dat.unfalsified_controller_index(next_controller);
+                u_dat.eta = u_dat.eta + u_dat.deta;
+            else
+                % Do nothing and continue with current active controller
+            end
+            
+        otherwise 
+            error('Chose an switching algorithm that is available; epsilon or LICLA')
     end
    
    
